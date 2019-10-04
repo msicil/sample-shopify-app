8619f34ba9a8f7c95f4c5a847313a2b1596a41bf
msicil <mikejoesis@gmail.com>
2019-10-04 16:55:39 -0400

    finished tutorial

--- next.config.js
+++ next.config.js
@@ -7,7 +7,21 @@ const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 module.exports = withCSS({
 	webpack: (config) => {
 		const env = { API_KEY: apiKey };
+		HACK_removeMinimizeOptionFromCssLoaders(config);
 		config.plugins.push(new webpack.DefinePlugin(env));
 		return config;
 	}
 });
+
+function HACK_removeMinimizeOptionFromCssLoaders(config) {
+	console.warn('HACK: Removing `minimize` option from `css-loader` entries in Webpack config');
+	config.module.rules.forEach((rule) => {
+		if (Array.isArray(rule.use)) {
+			rule.use.forEach((u) => {
+				if (u.loader === 'css-loader' && u.options) {
+					delete u.options.minimize;
+				}
+			});
+		}
+	});
+}
